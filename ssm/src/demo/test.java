37e288c187e45798af406c464e47a33618ade149
Sirk-chosenOne <18688192194@163.com>
2021-03-15 20:07:22 +0800

    真好

--- ssm/src/demo/test.java
+++ ssm/src/demo/test.java
@@ -4,5 +4,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("哈哈哈顶顶顶");
         System.out.println("我是你大爷！！！");
+        System.out.println("66666");
     }
 }
